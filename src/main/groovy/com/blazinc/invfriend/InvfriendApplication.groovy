d8fa13da6e7a4654c1ccdb928f3a2aabde395141
Gerson <tnii.gerson@gmail.com>
2018-12-15 15:10:56 -0800

    empty project

---
+++ src/main/groovy/com/blazinc/invfriend/InvfriendApplication.groovy
@@ -0,0 +1,14 @@
+package com.blazinc.invfriend
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class InvfriendApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run InvfriendApplication, args
+    }
+
+}
+
