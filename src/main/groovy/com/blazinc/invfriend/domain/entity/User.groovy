e1662338230a5fefe5c6d79993e2b7267e0343c4
Gerson <tnii.gerson@gmail.com>
2018-12-15 19:41:55 -0800

    user collection name changed

--- src/main/groovy/com/blazinc/invfriend/domain/entity/User.groovy
+++ src/main/groovy/com/blazinc/invfriend/domain/entity/User.groovy
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document
  * Entity partner class
  */
 @Canonical
-@Document(collection = 'partner')
+@Document(collection = 'user')
 class User {
 
     @Id
