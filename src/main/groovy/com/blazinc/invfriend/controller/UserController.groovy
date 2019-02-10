67fdc9ce0afd42a26480c9401b1f9ddcd34a6b29
Gerson <tnii.gerson@gmail.com>
2019-02-10 12:51:56 -0800

    temp new bot commands19

--- src/main/groovy/com/blazinc/invfriend/controller/UserController.groovy
+++ src/main/groovy/com/blazinc/invfriend/controller/UserController.groovy
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -51,6 +52,7 @@ class UserController {
 
     @ApiIgnore
     @GetMapping(value = '/')
+    @PostMapping(value = '/')
     RedirectView emptyUrlRedirect() {
         return new RedirectView('https://inv-friend.herokuapp.com/swagger-ui.html')
     }
